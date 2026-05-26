6d07f8619809542c5bd2a35bd3b65cfa4c3f35b7
Manon-Arc <112950709+Manon-Arc@users.noreply.github.com>
2026-05-26 16:48:26 +0200

    [TEST] test

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,9 +30,8 @@ dependencies {
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	testImplementation("io.kotest:kotest-assertions-core:6.1.11")
 	testImplementation("io.kotest:kotest-property:6.1.11")
+	testImplementation("io.kotest:kotest-runner-junit5:6.1.11")
 	testImplementation("io.mockk:mockk:1.14.9")
-	testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
-	testImplementation("io.kotest:kotest-assertions-core:5.9.1")
 }
 
 kotlin {
