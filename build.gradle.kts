378c601dc6c6593c79db24fa461d7cfbade63ac3
Manon-Arc <112950709+Manon-Arc@users.noreply.github.com>
2026-05-26 16:53:27 +0200

    [TEST] jacoco report

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
 	id("org.springframework.boot") version "3.5.14"
 	id("io.spring.dependency-management") version "1.1.7"
 	id("io.kotest") version "6.1.11"
+	id("jacoco")
+
 }
 
 group = "com.example"
@@ -42,4 +44,17 @@ kotlin {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+	finalizedBy(tasks.jacocoTestReport)
+
+}
+
+
+tasks.jacocoTestReport {
+	dependsOn(tasks.test)
+
+	reports {
+		xml.required.set(true)
+		html.required.set(true)
+		csv.required.set(false)
+	}
 }
